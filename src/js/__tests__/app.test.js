9c01b2090ebad34e261f610658cda4f424b62b87
macbook air <macbookair@MacBook-Air-macbook.local>
2020-05-26 14:32:38 +0300

    test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,10 +1,6 @@
 
 import Bowman from '../Bowman';
-import Daemon from '../Daemon';
-import Magician from '../Magician';
-import Swordsman from '../Swordsman';
-import Undead from '../Undead';
-import Zombie from '../Zombie';
+
 
 test('Bowman health', () => {
   const bowman = new Bowman('test');
